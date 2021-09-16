34258bef7089d6c5c11c369a01ce9fc84782e788
nkonyaek <nkonyaek@vcu.edu>
2021-09-16 15:37:30 -0400

    commit on 'master'

--- HW 09.16 Part 2.py
+++ HW 09.16 Part 2.py
@@ -8,7 +8,7 @@ while (len(str1)) % num2 == 0:
     k = 3
     res = [str1[i: j] for i in range(len(str1)) for j in range(i + 1, len(str1) + 1) if len(str1[i:j]) == k]
     print("The substrings of your input string is as follows : " + str(res))
-   
+    
     break
 else: 
         print("The integer is not a factor of the string length. Try again.") 
