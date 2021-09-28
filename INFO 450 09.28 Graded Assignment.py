277282502b0d3836d0bc0b97f4f2cfc69b4563d5
nkonyaek <nkonyaek@vcu.edu>
2021-09-27 20:19:49 -0400

    commit on 'master'

--- INFO 450 09.28 Graded Assignment.py
+++ INFO 450 09.28 Graded Assignment.py
@@ -1,12 +1,21 @@
-# Start a loop that will run until the user enters 'q' for quit.
+# Define empty responses dictionary
+responses_dict = {}
 
-state = input ("Enter a state or type q to quit: ")
-while state != 'q':
-        # Ask the user for their state of choice.
+# Set a flag to indicate that the user's responses are active.
+program_active = True
 
-    facts = input ("Enter a fact about the state or type q to quit: ")
-    if facts != 'q': 
-        new_state = input ("Enter a new state or type q to quit: ")
-else:
-
-    print ("You have decided to quit the program. Goodbye!")
+while program_active:
+    # Prompt for the person's state and fact of choice.
+    state = input("\nWhat is your state of choice? ")
+    fact = input("What is a fact about that state? ")
+    # Store the state in the dictionary.
+    responses_dict[state] = fact
+    # Find out if the user wants to add any more states or facts.
+    repeat = input("Would you like to add another state and fact? Type q if you wish to stop. (yes or q) ")
+    if repeat == 'q':
+       program_active = False
+       
+# Print back the states and facts.
+print("\n--- State & Facts Questionnaire Results ---")
+for state, fact in responses_dict.items():
+       print(f"A fact about the state of {state} is {fact}.")
