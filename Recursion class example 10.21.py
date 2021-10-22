f73339c4a2057b9cf4b615e6e8fd60524a274a27
nkonyaek <nkonyaek@vcu.edu>
2021-10-21 20:56:13 -0400

    commit on 'master'

--- Recursion class example 10.21.py
+++
@@ -1,9 +0,0 @@
-def gcd(a,b):
-    if(b==0):
-        return a
-    else:
-        return gcd(b,a%b)
-
-
-print (f"The GCD of the two numbers is : ",end="")
-print (gcd(225,85))
---
+++ Recursion exercises (10.21).py
@@ -0,0 +1,52 @@
+""""
+
+# Problem 1 
+
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+
+
+print (f"The GCD of the two numbers is : ",end="")
+print (gcd(225,85)) 
+
+"""
+
+
+
+"""
+
+# Problem 2 
+
+listA = [-5, 3, 6, 2, -8]
+
+def findMinimum(l):
+    if len(l) == 1:
+       return l[0]
+    
+    elif len(l) == 0:
+     return print("None")
+
+    else:
+       return min(l[0], findMinimum(l[1:]))
+
+findMinimum(listA) 
+
+"""
+
+
+
+# Problem 3
+
+
+def reverseString(s):
+    if s == "":
+        return s
+    else:
+        return reverseString(s[1:]) + s[0] 
+
+
+input_string = "python"
+reverseString(input_string)
