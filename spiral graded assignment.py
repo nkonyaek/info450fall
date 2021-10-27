35091526d42bfcda2cc5ae13cfb54333d7c8c9f2
nkonyaek <nkonyaek@vcu.edu>
2021-10-26 22:08:34 -0400

    commit on 'master'

--- spiral graded assignment.py
+++ spiral graded assignment.py
@@ -1,17 +1,21 @@
-def check_odd():
- n = int(input("Please enter an odd integer: ")
- if n % 2 != 0:
-    return n
- else:
-    return print("Error! The number you have entered is not odd.")
-
-check_odd()
-
 import numpy as np
-def spiral_array(A):
- A = np.array(A)
- out = []
- while(A.size):
-     out.append(A[0][::-1])    
-     A = A[1:][::-1].T         
- return np.concatenate(out)
+
+def spiral_array(n):
+ A = np.zeros(shape=(n,n))
+ center = (n - 1) / 2
+ row = center
+ column = center
+ print(A)
+ 
+
+
+ #while(A.size):
+    # out.append(A[0][::-1])    
+    # A = A[1:][::-1].T         
+ #return np.concatenate(out)
+
+n = int(input("Please enter an odd integer: "))
+if n % 2 != 0:
+       spiral_array(n)
+else:
+    print("The number you have entered is not odd. Please try again.")
